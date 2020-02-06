fa3b66ded31cc9cd5366ff567a89a6b151f60541
veroj_000 <veroj_000@JEREMY-LAPTOP>
2020-02-06 12:41:45 -0500

    Commit

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        private static string UName;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+
+            string input = Console.ReadLine();
+
+            UName = input; 
+
+            if (System.Text.RegularExpressions.Regex.IsMatch(input, "[a-zA-Z]")) 
+            {
+                Console.WriteLine("Hello " + UName + "!");
+            }
+
+            else 
+            {
+                Console.WriteLine("Please enter alphabetical letters only, and try again.");
+            }
+        }
+    }
+}
